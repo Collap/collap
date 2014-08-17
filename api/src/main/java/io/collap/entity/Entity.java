83fa28a79e33ed18be993be32910e8c6ac0f5c4d
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-08-17 02:37:30 +0200

    Added JSR305 (Nullable) support. Added an Entity class that is supposed to be the superclass of every collap entity. Made a mess out of the TODO.

---
+++ api/src/main/java/io/collap/entity/Entity.java
@@ -0,0 +1,25 @@
+package io.collap.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Entity {
+
+    private Long id;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+}
