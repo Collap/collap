8e99010f87bfc62e71f4114939acc47fdd87e619
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-09-01 00:48:41 +0200

    Update MainWrapper plugin to use bryg.

--- main-wrapper/src/main/java/io/collap/std/main_wrapper/MainWrapper.java
+++ main-wrapper/src/main/java/io/collap/std/main_wrapper/MainWrapper.java
@@ -1,28 +1,28 @@
 package io.collap.std.main_wrapper;
 
+import io.collap.bryg.environment.Environment;
+import io.collap.bryg.model.Model;
 import io.collap.controller.Wrapper;
 import io.collap.controller.communication.Request;
 import io.collap.controller.communication.Response;
-import io.collap.template.TemplateRenderer;
+import io.collap.controller.provider.BrygProvider;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainWrapper implements Wrapper {
 
-    private TemplateRenderer renderer;
+    private Environment bryg;
 
-    public MainWrapper (TemplateRenderer renderer) {
-        this.renderer = renderer;
+    public MainWrapper (BrygProvider brygProvider) {
+        bryg = brygProvider.getBryg ();
     }
 
     @Override
     public void execute (Response in, Request request, Response out) throws IOException {
-        Map<String, Object> model = new HashMap<> ();
-        model.put ("content", in.getContent ());
-        model.put ("head", in.getHead ());
-        renderer.renderAndWriteTemplate ("main", model, out.getContentWriter ());
+        Model model = bryg.createModel ();
+        model.setVariable ("headSrc", in.getHead ());
+        model.setVariable ("content", in.getContent ());
+        bryg.getTemplate ("main").render (out.getContentWriter (), model);
     }
 
 }
--- main-wrapper/src/main/java/io/collap/std/main_wrapper/MainWrapperModule.java
+++ main-wrapper/src/main/java/io/collap/std/main_wrapper/MainWrapperModule.java
@@ -1,12 +1,19 @@
 package io.collap.std.main_wrapper;
 
+import io.collap.bryg.EnvironmentConfigurator;
+import io.collap.bryg.EnvironmentCreator;
+import io.collap.bryg.ModuleSourceLoader;
+import io.collap.bryg.compiler.resolver.ClassResolver;
+import io.collap.bryg.environment.Environment;
+import io.collap.bryg.loader.SourceLoader;
+import io.collap.bryg.model.GlobalVariableModel;
+import io.collap.controller.provider.BrygProvider;
 import io.collap.plugin.Module;
-import io.collap.template.TemplateRenderer;
 import org.hibernate.cfg.Configuration;
 
-public class MainWrapperModule extends Module {
+public class MainWrapperModule extends Module implements BrygProvider, EnvironmentConfigurator {
 
-    private TemplateRenderer renderer;
+    private Environment bryg;
 
     @Override
     public void configureHibernate (Configuration configuration) {
@@ -15,12 +22,37 @@ public class MainWrapperModule extends Module {
 
     @Override
     public void initialize () {
-        renderer = new TemplateRenderer (this);
-        collap.getRootDispatcher ().setWrapper (new MainWrapper (renderer));
+        bryg = new EnvironmentCreator (collap, this).create ();
+        collap.getRootDispatcher ().setWrapper (new MainWrapper (this));
     }
 
     @Override
     public void destroy () {
+
+    }
+
+    @Override
+    public Environment getBryg () {
+        return bryg;
+    }
+
+    @Override
+    public SourceLoader getSourceLoader () {
+        return new ModuleSourceLoader (this);
+    }
+
+    @Override
+    public void configureConfiguration (io.collap.bryg.compiler.Configuration configuration) {
+
+    }
+
+    @Override
+    public void configureClassResolver (ClassResolver classResolver) {
+
+    }
+
+    @Override
+    public void configureGlobalVariableModel (GlobalVariableModel globalVariableModel) {
 
     }
 
