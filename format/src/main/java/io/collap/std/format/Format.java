cb34a46490b5fefadf20eb41ccf623836fd365dd
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-09-09 01:16:20 +0200

    Add capitalize function.

--- format/src/main/java/io/collap/std/format/Format.java
+++ format/src/main/java/io/collap/std/format/Format.java
@@ -18,4 +18,16 @@ public class Format {
         return String.format ("%,f", value);
     }
 
+    /**
+     * Capitalizes the first character of the string.
+     *
+     * This should only be used for single words; the
+     * method might be extended to support full sentences
+     * and in this case the API stays, but the functionality
+     * changes.
+     */
+    public String capitalize (String in) {
+        return Character.toUpperCase (in.charAt (0)) + in.substring (1);
+    }
+
 }
